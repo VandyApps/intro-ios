b1139e6a1732777c261d31f8b461b10c300928b8
--add <ethan@lookfamily.org>
2016-09-06 13:04:33 -0500

    adding directory for 2016-09-06 meeting

---
+++ 2016-09-06-meeting/2016-09-06-meeting.playground/Contents.swift
@@ -0,0 +1,73 @@
+/*
+ Created by Ethan Look. Demonstrated on September 6, 2016.
+ 
+ For more information, check out https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/GuidedTour.html
+ */
+
+// This is a comment in our code.
+
+/*
+ This is also a comment in our code!
+ */
+
+// TODO: Uncomment the below line by putting your cursor on the line and pressing Command + `/`
+// print("I'm uncommented!")
+
+// TODO: Create a variable called `catchphrase`, and assign it your favorite catchphrase!
+
+
+// TODO: Change the value of `catchphrase` to be a different catchphrase.
+
+
+// TODO: Create a constant called `name`, and assign it your name. See what happens when you try to reassign your name!
+
+
+// TODO: Declare a constant that is an implicit double.
+
+
+// TODO: Declare a constant that is an explicit integer.
+
+
+// TODO: Fill in the following to tell me how many apples you have.
+print("I have  apples in my backpack.")
+
+
+// TODO: Create an array of your favorite colors called faveColors.
+var faveColors: [String] = []
+
+// TODO: Add a new color to your array!
+
+
+// TODO: For each color, tell me the number of characters in the color.
+for color in faveColors {
+
+}
+
+// TODO: For each color, tell me if the color is longer or shorter than 4 characters. If it is 4 characters, say "Woohoo!".
+for color in faveColors {
+//    if () {
+//        
+//    } else if () {
+//        
+//    } else {
+//        
+//    }
+}
+
+// TODO: Create an optional integer variable called optionalNumber, and don't initialize it.
+
+
+//if let number = optionalNumber {
+//    print("The number exists!")
+//} else {
+//    print("The number does not exist. :(")
+//}
+
+// TODO: Initialize the value of optionalNumber!
+
+
+//if let number = optionalNumber {
+//    print("The number exists!")
+//} else {
+//    print("The number does not exist. :(")
+//}
